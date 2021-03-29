ea482e47bc1e8abf31b9d2bf77d7dc32f46e94ac
Leslie Enwerem <lenwerem@earthwarm.org>
2021-03-29 09:09:16 -0400

    191. Changing Node Env During Tests

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
 import cookieSession from "cookie-session";
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
-app.set('trust proxy', true); // Make express aware that traffic is being proxied to this service by ingress nginx/istio, and configure express to trust that proxy
+app.set("trust proxy", true); // Make express aware that traffic is being proxied to this service by ingress nginx/istio, and configure express to trust that proxy
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    // secure: true // NOTE: Disable security
+    // secure: process.env.NODE_ENV !== 'test' // NOTE: Disable security
   })
 );
 
@@ -25,7 +25,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', async (req, res) => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
