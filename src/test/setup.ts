ba94bbca55f8f07e394d1b6a893adfb8261e2b08
Leslie Enwerem <lenwerem@earthwarm.org>
2021-03-29 18:12:37 -0400

    196. Auth Helper Function

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,16 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
+
 import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -28,3 +38,21 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+// This signin function will only be available in the global testing context
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
