716b099681b2e420bf60182c3077e40a38766878
Leslie Enwerem <lenwerem@earthwarm.org>
2021-03-29 09:00:12 -0400

    190. Requiring Unique Emails

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -1,22 +1,21 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
 router.post(
-  '/api/users/signup',
+  "/api/users/signup",
   [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
-    body('password')
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters')
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -25,18 +24,20 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      console.log('Email in use');
-      return res.send({});
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
     await user.save();
 
     // Generate JWT without the user's password
-    const userJwt = jwt.sign({
-      id: user.id,
-      email: user.email
-    }, process.env.JWT_KEY!);
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
 
     // Set the jwt on a cookie-session object
     // Prevent typescript from complaining about req.session being undefined
