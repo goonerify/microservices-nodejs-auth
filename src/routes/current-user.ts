7487aeb1c404bc4776c5edc72782e834c503e2ba
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-03 09:48:35 -0400

    Adds todo for currentUser validation

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -9,6 +9,7 @@ router.get(
   "/api/users/currentuser",
   currentUser,
   (req: Request, res: Response) => {
+    // FIXME: We really should validate this user against the db, and not just trust the data from the cookie
     res.send({ currentUser: req.currentUser || null });
   }
 );
